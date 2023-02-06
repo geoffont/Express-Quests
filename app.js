f14a17a9d1d18541d3e6cfc71eb12bca0be78feb
geoffont <geoffroy.font@gmail.com>
2023-02-06 11:34:51 +0100

    express8

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 
-
 const app = express();
 
 app.use(express.json());
@@ -15,23 +14,44 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+
+const isItDwight = (req, res) => {
+  if (
+    req.body.email === "dwight@theoffice.com" &&
+    req.body.password === "123456"
+  ) {
+    res.send("Credentials are valid");
+  } else {
+    res.sendStatus(401);
+  }
+};
+app.post("/api/login", isItDwight);
+
 const movieHandlers = require("./movieHandlers");
+const { validateMovie } = require("./validators.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-const { hashPassword } = require("./auth.js");
-
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+app.post("/api/movies", verifyToken, validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", verifyToken, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", verifyToken, movieHandlers.deleteMovie);
+
+
+app.post("/api/users", verifyToken, hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", verifyToken, hashPassword, userHandlers.updateUser);
+app.delete("/api/users/:id", verifyToken, userHandlers.deleteUser);
 
 
 
@@ -44,5 +64,3 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
-
-
--- userHandlers.js
+++ userHandlers.js
@@ -55,6 +55,26 @@ const getUserById = (req, res) => {
     });
 };
 
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
 const postUser = (req, res) => {
   const { firstname, lastname, email, city, language, hashedPassword } =
     req.body;
@@ -116,6 +136,7 @@ const deleteUser = (req, res) => {
 module.exports = {
   getUsers,
   getUserById,
+  getUserByEmailWithPasswordAndPassToNext,
   postUser,
   updateUser,
   deleteUser,
